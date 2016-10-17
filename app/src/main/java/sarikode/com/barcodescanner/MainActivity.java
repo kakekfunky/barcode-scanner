58835b060c40400e0ff73b0abd86a682a4ee4162
wahyu <wahyu.agus.sw@gmail.com>
2016-10-17 22:41:16 +0700

    First commit source code barcode scanner untuk android

---
+++ app/src/main/java/sarikode/com/barcodescanner/MainActivity.java
@@ -0,0 +1,47 @@
+package sarikode.com.barcodescanner;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button tombol_scan = (Button) findViewById(R.id.btn_scan);
+
+        tombol_scan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                scan(v);
+            }
+        });
+    }
+
+    protected void scan(View view) {
+        new IntentIntegrator(this).initiateScan();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(result != null) {
+            if(result.getContents() == null) {
+                Toast.makeText(this, "Scan batal.", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "Data: " + result.getContents(), Toast.LENGTH_LONG).show();
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+}
